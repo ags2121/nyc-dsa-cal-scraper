c1e0b5c1bfce5520feb341e4613726d02fae2590
ags2121 <alexsilva88@gmail.com>
2019-12-06 15:25:02 -0500

    make friendlier

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import requests
-from datetime import datetime
 import pprint
 import pytz
 import html
+import sys
+import datetime
 
+def validate(date_text):
+    try:
+        datetime.datetime.strptime(date_text, '%m-%Y')
+    except ValueError:
+        raise ValueError("Incorrect data format, should be MM-YYYY")
 
-url = "https://www.socialists.nyc/api/open/GetItemsByMonth?month=04-2019&collectionId=57a26db33e00be2197c966ad"
+month = sys.argv[-1]
+
+validate(month)
+
+url = "https://www.socialists.nyc/api/open/GetItemsByMonth?month=%s&collectionId=57a26db33e00be2197c966ad" % month
+print(url)
 # % dict(
 #     url="https://www.socialists.nyc/api/open",
 #     endpoint="/GetItemsByMonth",
@@ -14,7 +25,7 @@ url = "https://www.socialists.nyc/api/open/GetItemsByMonth?month=04-2019&collect
 timezoneLocal = pytz.timezone('America/New_York')
 def convert_ts(ts):
     utc = pytz.utc
-    return pytz.utc.localize(datetime.utcfromtimestamp(ts / 1000)).astimezone(timezoneLocal)
+    return pytz.utc.localize(datetime.datetime.utcfromtimestamp(ts / 1000)).astimezone(timezoneLocal)
 
 str_temp ="""%s
 %s
